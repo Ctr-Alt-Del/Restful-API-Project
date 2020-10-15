1b26f5f15cd010925afdf3e0f33d3d761f8103c5
AthiM1 <mbondwana.a.s@gmail.com>
2020-10-15 11:36:07 +0200

    Added orders routes and orders mordel

---
+++ Project/backend/routes/orders.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../routes/models/order');
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    })
+});
+
+router.post('/', (req, res) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'Orders were created',
+        order: order
+    });
+});
+
+router.get('/:orderId', (req, res) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res) => {
+    res.status(200).json({
+        message: 'Order deleted',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router;
