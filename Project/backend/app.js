3cc132df2e594243e6e44e4b61bfa79f34d739d3
Leigh-Jean <ljxrustin@gmail.com>
2020-10-15 16:00:03 +0200

    Update app.js

--- Project/backend/app.js
+++ Project/backend/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 
 //@desc define routes
-// app.use('/api/orders', orderRoutes);
+app.use('/api/orders', orderRoutes);
 app.use('/api/auth',authRoutes );
 app.use('/api/products', productRoutes);
 app.use('/api/users', usersRoutes);
